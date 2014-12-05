201c0a23e069476ca36d8afb66a3e4dee565fa54
Meredith Rawls <meredith.rawls@gmail.com>
2014-12-05 12:32:57 -0700

    new parameter order + option for unfolded data

--- 2Dhistgrid.py
+++ 2Dhistgrid.py
@@ -24,25 +24,23 @@ Also plots a RV curve (data + fit) and light curve (data + fit) in the upper rig
 '''
 
 # Number of fit parameters and their names
-KIC = '9246715'
-npar = 10 #please make it even for sanity
-#parlabels = ['$P_{orb}$','$e \cos \omega$','$e \sin \omega$','$i$','$M_1$','$T_1$','$T_2/T_1$','$R_1/a$','$R_2/a$','$K_1$']
-#pars = ['period', 'oc', 'os', 'inclination', 'pm', 'T1', 'temprat', 'q1', 'q2', 'pk'] #ELC names
-#parindices = [1, 3, 4, 5, 6, 7, 8, 9, 10, 11] #ELC index
-parlabels = ['$e \cos \omega$','$e \sin \omega$','$i$','$M_1$','$T_1$','$T_2/T_1$','$R_1/a$','$R_2/a$','$K_1$','$P_{orb}$']
-pars = ['oc', 'os', 'inclination', 'pm', 'T1', 'temprat', 'q1', 'q2', 'pk', 'period'] #ELC names
-parindices = [2, 3, 4, 5, 6, 7, 8, 9, 10, 16] #ELC index
+filepath = '../../RG_ELCmodeling/9246715/trial4_fromseismo_super/'
+folded = False # if you used itime=2 in ELC, set this to False. otherwise, True.
+npar = 10 #please make it even for sanity ... actually maybe odd is ok?
+parlabels = ['$P_{orb}$', '$e \cos \omega$','$e \sin \omega$','$i$','$M_1$','$T_1$','$T_2/T_1$','$R_1/a$','$R_2/a$','$K_1$']
+pars = ['period', 'oc', 'os', 'inclination', 'pm', 'T1', 'temprat', 'q1', 'q2', 'pk'] #ELC names
+parindices = [2, 4, 5, 6, 7, 8, 9, 10, 11, 12] #ELC index
 nbins = 50 # number of histogram bins (same in both dimensions)
 
 # Files that have stuff in them
-gridloop = KIC + '/trial3_switch/gridloop.opt'
-parfile = KIC + '/trial3_switch/generation.all'
-lcmodel = KIC + '/trial3_switch/modelU.mag'
-lcdata = KIC + '/trial3_switch/ELCdataU.fold'
-rv1model = KIC + '/trial3_switch/star1.RV'
-rv1data = KIC + '/trial3_switch/ELCdataRV1.fold'
-rv2model = KIC + '/trial3_switch/star2.RV'
-rv2data = KIC + '/trial3_switch/ELCdataRV2.fold'
+gridloop = filepath + 'gridloop.opt'
+parfile = filepath + 'generation.all'
+lcmodel = filepath + 'modelU.mag'
+lcdata = filepath + 'ELCdataU.fold'
+rv1model = filepath + 'star1.RV'
+rv1data = filepath + 'ELCdataRV1.fold'
+rv2model = filepath + 'star2.RV'
+rv2data = filepath + 'ELCdataRV2.fold'
 
 # Color map for 2D histogram "contours"
 cx = cubehelix.cmap(reverse=True, maxLight=0.7, start=0., rot=0.5)
@@ -56,6 +54,21 @@ magdim = 9.54
 magbright = 9.21
 rvmin = -59
 rvmax = 59
+
+# Function to fold stuff if it isn't folded already
+def phasecalc(times, period=100, BJD0=2454833):
+	phases = []
+	cycles = []
+	for i in range(0, len(times)):
+		fracP = (times[i] - BJD0) / period
+		if fracP < 0:
+			phases.append(fracP % 1)
+			cycles.append(int(fracP))
+		else:
+			phases.append(fracP % 1)
+			cycles.append(int(fracP) + 1)
+		#print(fracP, phases[i])
+	return np.array(phases)
 
 # Get axis limits for each histogram box from the gridloop.opt file
 parmin = []
@@ -142,7 +155,7 @@ for row in range(0, npar):
 		if col == 0 and row != 0:
 			plt.ylabel(parlabels[row], rotation='horizontal', labelpad=30) #y-axis labels
 
-# Upper right corner: RV plot
+# Open stuff necessary for LC + RV plot
 with open(rv1data) as f1:
 	phase_rv1dat, rv1dat = np.loadtxt(f1, comments='#', dtype=np.float64, usecols=(0,1), unpack=True)
 with open(rv2data) as f1:
@@ -151,6 +164,49 @@ with open(rv1model) as f1:
 	phase_rv1mod, rv1mod = np.loadtxt(f1, comments='#', dtype=np.float64, usecols=(0,1), unpack=True)
 with open(rv2model) as f1:
 	phase_rv2mod, rv2mod = np.loadtxt(f1, comments='#', dtype=np.float64, usecols=(0,1), unpack=True)
+with open(lcmodel) as f1:
+	phase_lcmod, mag_lcmod = np.loadtxt(f1, comments='#', dtype=np.float64, usecols=(0,1), unpack=True)
+with open(lcdata) as f1:
+	phase_lcdat, mag_lcdat = np.loadtxt(f1, comments='#', dtype=np.float64, usecols=(0,1), unpack=True)
+
+if folded == False:
+# FOLD STUFF so phases are actually phases ... and then sort all the arrays.
+	with open(filepath+'ELC.out') as f:
+		for i, row in enumerate(f):
+			if i == 27: # 28th row
+				columns = row.split()
+				period = float(columns[0]) # 1st column
+			#if i == 38: # 39th row, i.e. T0	# this one has a funny zeropoint (ok if circular)
+			if i == 133: # 134th row, i.e. Tconj # this one puts primary eclipse at phase 0
+				columns = row.split()
+				Tconj = float(columns[0]) #1st column
+	Tconj = Tconj + 0.5*period
+	phase_lcmod = phasecalc(phase_lcmod, period=period, BJD0=Tconj)
+	phase_lcdat = phasecalc(phase_lcdat, period=period, BJD0=Tconj)
+	phase_rv1mod = phasecalc(phase_rv1mod, period=period, BJD0=Tconj)
+	phase_rv2mod = phasecalc(phase_rv2mod, period=period, BJD0=Tconj)
+	phase_rv1dat = phasecalc(phase_rv1dat, period=period, BJD0=Tconj)
+	phase_rv2dat = phasecalc(phase_rv2dat, period=period, BJD0=Tconj)
+	p1 = phase_lcmod.argsort()
+	p2 = phase_lcdat.argsort()
+	p3 = phase_rv1mod.argsort()
+	p4 = phase_rv2mod.argsort()
+	p5 = phase_rv1dat.argsort()
+	p6 = phase_rv2dat.argsort()
+	phase_lcmod = phase_lcmod[p1]
+	phase_lcdat = phase_lcdat[p2]
+	phase_rv1mod = phase_rv1mod[p3]
+	phase_rv2mod = phase_rv2mod[p4]
+	phase_rv1dat = phase_rv1dat[p5]
+	phase_rv2dat = phase_rv2dat[p6]
+	mag_lcmod = mag_lcmod[p1]
+	mag_lcdat = mag_lcdat[p2]
+	rv1mod = rv1mod[p3]
+	rv2mod = rv2mod[p4]
+	rv1dat = rv1dat[p5]
+	rv2dat = rv2dat[p6]
+
+# Upper right corner: RV plot
 fig2 = plt.subplot2grid((npar*2,npar*2), (0,npar+1), rowspan=int((npar-1)/2), colspan=npar-1)
 fig2.axis([phasemin,phasemax,rvmin,rvmax])
 fig2.set_xticklabels(())
@@ -166,10 +222,6 @@ fig2.legend(loc='upper right', frameon=False, prop={'size':20})
 plt.ylabel('RV (km s$^{-1}$)')
 
 # Upper right corner: Light curve plot
-with open(lcmodel) as f1:
-	phase_lcmod, mag_lcmod = np.loadtxt(f1, comments='#', dtype=np.float64, usecols=(0,1), unpack=True)
-with open(lcdata) as f1:
-	phase_lcdat, mag_lcdat = np.loadtxt(f1, comments='#', dtype=np.float64, usecols=(0,1), unpack=True)
 fig3 = plt.subplot2grid((npar*2,npar*2), (int((npar-1)/2),npar+1), rowspan=(npar-1)-int((npar-1)/2), colspan=npar-1)
 fig3.axis([phasemin,phasemax,magdim,magbright])
 fig3.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
